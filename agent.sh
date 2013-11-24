0a60fd0f938cddabf61f78ee1088d660a316cd06
Alexei Guevara <alexguev@gmail.com>
2013-11-23 23:11:39 -0500

    delete logging

--- agent.sh
+++ agent.sh
@@ -2,10 +2,6 @@
 
 AGENT_CONFIG=$TEAM_CITY_INSTALL_DIR/TeamCity/buildAgent/conf/buildAgent.properties
 
-echo $0
-echo $1
-echo $2
-
 sed -i.bak "s@serverUrl=http://localhost:8111/@serverUrl=$1@g" $AGENT_CONFIG
 sed -i.bak "s@name=Default Agent@name=$2@g" $AGENT_CONFIG
 
